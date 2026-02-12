9306b0082439787c0f26815060b9c57dedddcf30
finteger <nashassociatesinc@gmail.com>
2026-02-11 18:55:46 -0700

    Update

--- pytest/todo_list/test_todo_list.py
+++ pytest/todo_list/test_todo_list.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 import time
 import http.server
 import socketserver
@@ -33,29 +33,40 @@ def stop_server():
         print('Server is shutdown')
         
 
-def test_add_todo_item():
-    print("\n===Test: Add To-Do Item===")
-     
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True, slow_mo=500)
-        page = browser.new_page()
-        
-        #open the app
-        page.goto("http://localhost:8000")
-        
-        #type in the task
-        print("Adding new task...")
-        page.get_by_test_id('todo-input').fill("Buy groceries")
-        
-        time.sleep(1)
-        browser.close()
+def test_add_todo_item(page: Page):
+    print('\n=== Running test: Add To-Do Item ===')
+    
+    #navigate to the app
+    page.goto('http://localhost:8000/todo_list.html')
+    
+    print('Adding a new task...')
+    #find the input element and fill it in with a new task
+    page.get_by_test_id('todo-input').fill('Buy groceries')
+    
+    #click the submit button 
+    page.get_by_role("button", name="Submit").click()
+    
+    page.wait_for_timeout(500)
+    
+    #verify that the task appears on the UL list
+    todo_items = page.get_by_test_id('todo-item')
+    assert todo_items.count() == 1, "Should have 1 to-do item after adding"
+    
+    #verify the counter updates
+    total_count = page.locator('#total-count').text_content()
+    assert total_count == 1, f'Total count should be 1 after adding, but got {total_count}' 
+    
+    print("Task added succesfully!")
+    
+    page.wait_for_timeout(500)
+    
         
         
 #run our suite of tests 
 def run_all_tests():
     try:
         test_add_todo_item()    
-    #to-do implement tests
+        #to-do implement tests
     except AssertionError as e:
         print(f'Test failed: {e}')
     except Exception as e:
